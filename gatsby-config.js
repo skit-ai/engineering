d4a71d3aec64fa933f8a59a8800f9bfc1c951104
Shantanu Sharma <shantanu28sharma@gmail.com>
2020-06-16 11:24:54 +0530

    feat: initial write

---
+++ gatsby-config.js
@@ -0,0 +1,81 @@
+require(`dotenv`).config({
+  path: `.env`,
+});
+
+module.exports = {
+  siteMetadata: {
+    siteTitle: `Engineering at Vernacular.ai`,
+    siteTitleAlt: `Engineering | Vernacular.ai`,
+    siteHeadline: `Engineering | Vernacular.ai`,
+    siteUrl: `https://vernacular-ai.github.io/ml`,
+    siteDescription: `Notes from the Engineering team at Vernacular.ai`,
+    siteImage: ``,
+    author: ``,
+  },
+  plugins: [
+    {
+      resolve: `@lekoarts/gatsby-theme-minimal-blog`,
+      // See the theme's README for all available options
+      options: {
+        navigation: [
+          {
+            title: `Blog`,
+            slug: `/blog`,
+          },
+          {
+            title: `About`,
+            slug: `/about`,
+          },
+          {
+            title: `Team`,
+            slug: `/team`,
+          },
+        ],
+        externalLinks: [
+          {
+            name: `Twitter`,
+            url: `https://twitter.com/lekoarts_de`,
+          },
+          {
+            name: `Instagram`,
+            url: `https://www.instagram.com/lekoarts.de/`,
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+      },
+    },
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
+        short_name: `minimal-blog`,
+        description:
+          `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#6B46C1`,
+        display: `standalone`,
+        icons: [
+          {
+            src: `/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-netlify`,
+  ],
+};
