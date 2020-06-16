1988d378e7c7659132531dadae6372afae1a8fd6
Shantanu Sharma <shantanu28sharma@gmail.com>
2020-06-16 11:32:25 +0530

    updated URL

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
     siteTitle: `Engineering at Vernacular.ai`,
     siteTitleAlt: `Engineering | Vernacular.ai`,
     siteHeadline: `Engineering | Vernacular.ai`,
-    siteUrl: `https://vernacular-ai.github.io/ml`,
+    siteUrl: `https://vernacular-ai.github.io/`,
     siteDescription: `Notes from the Engineering team at Vernacular.ai`,
     siteImage: ``,
     author: ``,
@@ -55,8 +55,7 @@ module.exports = {
       options: {
         name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
         short_name: `minimal-blog`,
-        description:
-          `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
